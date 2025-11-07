ecd8c1cf77aaf2ed2d45c7d1c9ad3357aa8ade80
ViktorThorsen <viktorthorsen92@gmail.com>
2025-11-07 11:06:01 +0100

    Fixing menu

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
-import MainPage from './MainPage/index.tsx'
+import MainPage from './pages/MainPage/index.tsx'
+import NavBar from './components/NavBar/index.tsx'
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { appTheme } from './theme/theme.tsx';
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={appTheme}>
       <CssBaseline />
+      <NavBar/>
       <MainPage />
     </ThemeProvider>
   </StrictMode>,
