affffbff681317129f311324a088cf4baba0b426
okzScripts <oskar.krantz90@gmail.com>
2025-11-17 22:38:36 +0100

    work on user and roles, WIP

---
+++ server/helpers/SessionExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace server;
+
+public class SessionExtensions
+{
+    public static UserRole? GetUserRole(this ISession session)
+    {
+        var role = session.GetString("UserRole");
+
+        if (role != null && Enum.TryParse<UserRole>(role, out var parsedRole))
+            return parsedRole;
+
+        return null;
+    }
+}
