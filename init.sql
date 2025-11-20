1b93bddfca5dc486e59dd41a38a5d4bb90ad6eb6
nullisokz <oskar.krantz90@gmail.com>
2025-11-20 11:42:40 +0100

    init sql

--- init.sql
+++ init.sql
@@ -2,7 +2,8 @@
 CREATE TABLE users (
     id   SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE categories (
